3d7b9a27164956cc6156383be2de6f073d4ab79f
Eric Lawrence <elawrence@commonsense.org>
2020-04-14 21:00:21 -0500

    Minor style ups

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
  */
 
 const init = async () => {
-  console.log(`%c ::: init :::`, 'color:#ddd; background: #eee;')
+  console.log(`%c ::: init ::: `, 'color:#fff; background: #ddd; border-radius: 3px; text-shadow: -1px -1px #ccc; line-height: 1.25rem; padding: 0 1rem; display: block; width: 100%;')
   return
 }
 
